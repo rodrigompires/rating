804ed2f3b4ace2e9c42b48d3bff8a539dff502e2
Rodrigo Pires <digo0203@gmail.com>
2023-07-26 18:05:19 -0300

    Updates

---
+++ precache-manifest.253a29b9ead6c6929e76106d98008a89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "792992213efb7ac587d4",
+    "url": "/rating/static/css/main.2f30e552.chunk.css"
+  },
+  {
+    "revision": "792992213efb7ac587d4",
+    "url": "/rating/static/js/main.79299221.chunk.js"
+  },
+  {
+    "revision": "d8dfaa87329dba9d8831",
+    "url": "/rating/static/js/1.d8dfaa87.chunk.js"
+  },
+  {
+    "revision": "8f0101144d001a7bc349",
+    "url": "/rating/static/js/runtime~main.8f010114.js"
+  },
+  {
+    "revision": "e1ac14204e8aba3596472a748fa7fc26",
+    "url": "/rating/static/media/icon-star.e1ac1420.svg"
+  },
+  {
+    "revision": "cb8e07d4b2e2030ca91a82f5aae61dcd",
+    "url": "/rating/static/media/illustration-thank-you.cb8e07d4.svg"
+  },
+  {
+    "revision": "dfda5284b29875ba74afb805e7731517",
+    "url": "/rating/static/media/image-1.dfda5284.svg"
+  },
+  {
+    "revision": "2bc908c632127009770ac69264919099",
+    "url": "/rating/index.html"
+  }
+];
